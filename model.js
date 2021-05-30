36b87bd2cf3f81aeb7f19523434d297bf62947d0
cppizarro <cppizarro@miuandes.cl>
2021-05-29 21:27:10 -0400

    cosas de prueba

---
+++ model.js
@@ -0,0 +1,10 @@
+initModel = {
+    name: 0,
+    temp: 0,
+    max: 0,
+    min: 0
+}
+
+module.exports = {
+    initModel
+}
--- view.js
+++ view.js
@@ -14,3 +14,52 @@ function getTitle(){
         )
     )
 }
+
+function getTable(model){
+    
+    const {name} = model
+    const {temp} = model
+    const {max} = model
+    const {min} = model
+    
+    return [
+        {
+            'name': name,
+            'temp': temp,
+            'max': max,
+            'min': min
+        }
+    ]
+}
+
+function getAction(){
+    return inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Select action:',
+                name: 'action',
+                choices: ['Add City', 'Update City', 'Delete City']
+
+            }
+        ])
+}
+
+function Action(action){
+    if (action === 'Add City'){
+        return inquirer.prompt([
+            {
+                type: 'Input',
+                message: 'Location?',
+                name: 'location',
+            }
+        ])
+    }
+
+    else if (action === 'Update City'){
+        return 0
+    }
+
+    else if (action === 'Delete City'){
+        return 0
+    }
+}
