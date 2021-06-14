2d99cd2ec8327347ff795076193fd2f6fad16cca
cppizarro <cppizarro@miuandes.cl>
2021-06-14 15:00:23 -0400

    modificacion update

--- update.js
+++ update.js
@@ -7,14 +7,16 @@ function api(location){
 
 function addCity(name_city, model){
     city = {name: name_city, temp:0, max:0, min:0}
-    model.push(city)
-    return model    
+
+    const temp_model = model
+
+    temp_model.cities.push(city)
+    temp_model.names.push(name_city)
+    return temp_model    
 }
 
 function updateCity(name_city, model){
-    city = {name: name_city, temp:0, max:0, min:0}
-    model.push(city)
-    return model 
+    
 }
 
 function deleteCity(name_city, model){
@@ -22,26 +24,20 @@ function deleteCity(name_city, model){
 }
 
 
-function update(input, name_city, model){
-    
-    const {name_city} = model
-    const {temp} = input
-    const {max} = input
-    const {min} = input
-    
-    if (input === 'Add City'){
-        addCity(name_city, model)
+function update(action, name_city, model){
+    if (action === 'Add City'){
+        return addCity(name_city, model)
     }
 
-    else if (input === 'Update City'){
-        updateCity(name_city, model)
+    else if (action === 'Update City'){
+        return updateCity(name_city, model)
     }
 
-    else if (input === 'Delete City'){
-        deleteCity(name_city, model)
+    else if (action === 'Delete City'){
+        return deleteCity(name_city, model)
     }
-    return {
-        ...model,
-        
-    }
+}
+
+module.exports = {
+    update
 }
